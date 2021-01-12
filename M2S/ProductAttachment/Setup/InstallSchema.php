0e281548294cd5bc66008667312442f140b4ecd6
piotrwx <piotr.wlosekx@gmail.com>
2021-01-12 21:32:33 +0100

    M2S ProductAttachment ver.0.0.2 - Create table

---
+++ M2S/ProductAttachment/Setup/InstallSchema.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace M2S\ProductAttachment\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $table = $setup->getConnection()->newTable('m2s_productattachment')
+        ->addColumn(
+            'id',
+            Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'nullable' => false, 'primary' => true],
+            'ID'
+        )->addColumn(
+            'product_id',
+            Table::TYPE_INTEGER,
+            null,
+            ['nullable' => false],
+            'Product id'
+        )->addColumn(
+            'attachment_img',
+            Table::TYPE_TEXT,
+            null,
+            ['nullable' => false],
+            'attachment'
+        )->addColumn(
+            'status',
+            Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false],
+            '0 = disable / 1 = enable'
+        )->addColumn(
+            'created_at',
+            Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => Table::TIMESTAMP_INIT],
+            'added at'
+        )->setComment('M2S Product Attachment');
+        $setup->getConnection()->createTable($table);
+
+        $setup->endSetup();
+    }
+}
